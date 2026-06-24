4cd4da909f3a856c84e895ae3ce146d81130455a
prathamesh gorad <prathameshgorad96@gmail.com>
2026-06-24 20:34:23 +0530

    changes

--- mernempproject/emps_frontend/src/component/Navbar.jsx
+++ mernempproject/emps_frontend/src/component/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <>
     <nav
-        className="navbar navbar-expand-sm navbar-dark bg-dark"
+        className="navbar navbar-expand-sm navbar-dark bg-primary"
     >
-        <NavLink className="navbar-brand" to="/">Emp</NavLink>
+        <NavLink className="navbar-brand" to="/">Navbar</NavLink>
         <button
             className="navbar-toggler d-lg-none"
             type="button"
@@ -24,13 +24,33 @@ const Navbar = () => {
                         >Home <span className="visually-hidden">(current)</span></NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink className="nav-link" to="/add">add Emp</NavLink>
+                    <NavLink className="nav-link" to="#">Link</NavLink>
                 </li>
-                
+                <li className="nav-item dropdown">
+                    <NavLink
+                        className="nav-link dropdown-toggle"
+                        to="#"
+                        id="dropdownId"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                        >Dropdown</NavLink>
+                    
+                </li>
             </ul>
-            
+            <form className="d-flex my-2 my-lg-0">
+                <input
+                    className="form-control me-sm-2"
+                    type="text"
+                    placeholder="Search"
+                />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                    Search
+                </button>
+            </form>
         </div>
     </nav>
+    
     
     </>
   )
