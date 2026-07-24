f495003260194c848f7a58863e6bacca6fe7b3f2
prathamesh gorad <prathameshgorad96@gmail.com>
2026-07-24 22:36:12 +0530

    new

---
+++ smart-notes/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import {useRef} from 'react'
+function App() {
+  const inputRef = useRef(null);
+  useEffect(()=>{
+    inputRef.current.focus()
+  },[])
+  return (
+    
+    <div>
+      <h1>smart notes</h1>
+
+      <input ref={inputRef}type="text" placeholder="enter note title" /><br /><br />
+      <input ref={inputRef}type="description" placeholder="enter note title" />
+    </div>
+  )
+}
+
+export default App
